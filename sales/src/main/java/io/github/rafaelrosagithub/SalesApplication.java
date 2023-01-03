f81deaacb1fac784659f42e7732d6b1a549715c9
Rafael <rafael_unis@hotmail.com>
2023-01-03 14:17:06 -0300

    Created project

---
+++ sales/src/main/java/io/github/rafaelrosagithub/SalesApplication.java
@@ -0,0 +1,11 @@
+package io.github.rafaelrosagithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
+    }
+}
