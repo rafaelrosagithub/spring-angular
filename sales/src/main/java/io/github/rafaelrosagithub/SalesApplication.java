9fdc9408d210113642ef7eb6c1d1f190e069c0ea
Rafael <rafael_unis@hotmail.com>
2023-01-03 14:25:09 -0300

    Hello World with @RestController

--- sales/src/main/java/io/github/rafaelrosagithub/SalesApplication.java
+++ sales/src/main/java/io/github/rafaelrosagithub/SalesApplication.java
@@ -2,9 +2,17 @@ package io.github.rafaelrosagithub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SalesApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!!!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(SalesApplication.class, args);
     }
