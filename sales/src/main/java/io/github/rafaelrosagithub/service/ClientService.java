242b00f63290ac193d92dbe7644ca6b030226ee7
Rafael <rafael_unis@hotmail.com>
2023-01-05 17:50:13 -0300

    Dependency injetcion via @Autowired

--- sales/src/main/java/io/github/rafaelrosagithub/service/ClientService.java
+++ sales/src/main/java/io/github/rafaelrosagithub/service/ClientService.java
@@ -8,16 +8,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClientService {
 
-    private ClientRepository repository;
-
     @Autowired
-    public ClientService(ClientRepository repository) {
-        this.repository = repository;
-    }
+    private ClientRepository repository;
 
     public void saveClient(Client client) {
         validateClient(client);
-        this.repository.save(client);
+        repository.save(client);
     }
 
     public void validateClient(Client client) {
