299186d60970dd19b2888248af7f950dd6bc9d7a
Rafael <rafael_unis@hotmail.com>
2023-01-10 21:59:59 -0300

    Custom configuration annotations

--- sales/src/main/java/io/github/rafaelrosagithub/MyConfiguration.java
+++ sales/src/main/java/io/github/rafaelrosagithub/MyConfiguration.java
@@ -3,10 +3,9 @@ package io.github.rafaelrosagithub;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Development
 public class MyConfiguration {
 
     @Bean(name = "profileDev")
