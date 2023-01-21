aa46f97c4a52a85ee5ff297af20e7fa117231bbf
Rafael <rafael_unis@hotmail.com>
2023-01-21 19:39:11 -0300

    Handling api errors

---
+++ customers/src/main/java/org/example/customers/rest/ApplicationControllerAdvice.java
@@ -0,0 +1,23 @@
+package org.example.customers.rest;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Object handleValidationErrors(MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<String> messages = bindingResult.getAllErrors()
+                .stream()
+                .map(objectError -> objectError.getDefaultMessage())
+                .collect(Collectors.toList());
+    }
+
+}
