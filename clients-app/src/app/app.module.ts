5f79a65fefc12056e0d65b2fdc391b84df3e2952
Rafael <rafael_unis@hotmail.com>
2023-01-23 14:53:07 -0300

    Creating first modulo of angular

--- clients-app/src/app/app.module.ts
+++ clients-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { TestAngularComponent } from './test-angular/test-angular.component'
+import { ClientsModule } from './clients/clients.module'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TestAngularComponent } from './test-angular/test-angular.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClientsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
