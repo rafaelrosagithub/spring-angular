231fdf35a8890a123a0db8a89157ebfc4a1b3d8f
Rafael <rafael_unis@hotmail.com>
2023-01-24 19:14:57 -0300

    Configuring navigation through routes

---
+++ clients-app/src/app/clients/clients.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientsRoutingModule } from './clients-routing.module';
+import { ClientsFormComponent } from './clients-form/clients-form.component';
+
+
+@NgModule({
+  declarations: [ClientsFormComponent],
+  imports: [
+    CommonModule,
+    ClientsRoutingModule
+  ]
+})
+export class ClientsModule { }
