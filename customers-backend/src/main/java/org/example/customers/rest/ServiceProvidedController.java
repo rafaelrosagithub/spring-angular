4b17a5e09e3cc70dee8926794c9cd1e358654463
Rafael <rafael_unis@hotmail.com>
2023-02-21 18:03:54 -0300

    Allowing CORS

--- customers-backend/src/main/java/org/example/customers/rest/ServiceProvidedController.java
+++ customers-backend/src/main/java/org/example/customers/rest/ServiceProvidedController.java
@@ -18,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/services-provided")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:4200")
 public class ServiceProvidedController {
 
     private final ClientRepository clientRepository;
