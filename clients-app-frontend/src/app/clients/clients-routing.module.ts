5857f9fd27be33efe0d9dcb513e007d51b7e6554
Rafael <rafael_unis@hotmail.com>
2023-03-01 22:07:40 -0300

    Child routes and different templates

--- clients-app-frontend/src/app/clients/clients-routing.module.ts
+++ clients-app-frontend/src/app/clients/clients-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
 import { ClientsFormComponent } from './clients-form/clients-form.component'
 import { ClientsListComponent } from './clients-list/clients-list.component';
 
 const routes: Routes = [
+  { path: 'clients', component: LayoutComponent }
   { path: 'clients-form', component: ClientsFormComponent },
   { path: 'clients-form/:id', component: ClientsFormComponent },
   { path: 'clients-list', component: ClientsListComponent }
