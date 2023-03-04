d4a5983390537843c91f8442760ecd59caa1935a
Rafael <rafael_unis@hotmail.com>
2023-03-04 16:42:50 -0300

    Created API de users

---
+++ customers-backend/src/main/java/org/example/customers/rest/UserController.java
@@ -0,0 +1,23 @@
+package org.example.customers.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.example.customers.model.entity.User;
+import org.example.customers.model.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody @Valid User user) {
+        userRepository.save(user);
+    }
+}
