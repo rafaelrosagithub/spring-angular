b54e07fc2134a190dd3655dac0cd6339e5f67f58
Rafael <rafael_unis@hotmail.com>
2023-03-04 15:30:40 -0300

    User entity modeling

---
+++ customers-backend/src/main/java/org/example/customers/model/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.customers.model.repository;
+
+import org.example.customers.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
