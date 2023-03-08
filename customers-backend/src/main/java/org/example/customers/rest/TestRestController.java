bc1987e7427c83dfd21a2fb0e980f9d7a0ffa205
Rafael <rafael_unis@hotmail.com>
2023-03-07 21:10:10 -0300

    Scrolling through list of files

--- customers-backend/src/main/java/org/example/customers/rest/TestRestController.java
+++ customers-backend/src/main/java/org/example/customers/rest/TestRestController.java
@@ -8,13 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
 @RestController
 @RequestMapping("/test")
 public class TestRestController {
 
     @GetMapping
-    public String test() {
+    public String test() throws ParseException {
         System.out.println("Test");
+
+        LocalDateTime now = LocalDateTime.now();
+
+
+        Timestamp date2 = Timestamp.valueOf(now);
+
         SFTP();
         return "test";
     }
@@ -23,7 +37,7 @@ public class TestRestController {
         String localPath = "D:/FTP";
         String fileName = "filenovo.txt";
         String sftpHost = "192.168.0.110";
-        String sftpPath = "/TESTE";
+        String sftpPath = "/OficiosECM";
         String sftpPort = "22";
         String sftpUser = "teste";
         String sftpPassword = "teste";
@@ -46,9 +60,38 @@ public class TestRestController {
             /**
              * TODO
              */
-            System.out.println("Copying file to Host SFTP");
-            channelSftp.put(localPath + "/" + fileName, sftpPath);
-            System.out.println("Copied file to Host SFTP");
+            Vector<ChannelSftp.LsEntry> fileList = channelSftp.ls(sftpPath);
+            List<String> list = new ArrayList<>();
+            for (ChannelSftp.LsEntry item : fileList) {
+
+                if (item.getFilename().contains("_DONE")) {
+                    String n = item.getFilename();
+                    list.add(Arrays.toString(n.split("_")));
+                }
+
+                System.out.println(item.getFilename().toString());
+            }
+
+            System.out.println(list);
+            list.forEach(n -> System.out.println(n));
+
+
+
+
+
+//            if (fileList.size() > 0) {
+//                System.out.println("QTD files: " + fileList.size());
+//                fileList.stream().forEach(n -> {
+//                   System.out.println(n);
+//               });
+//            } else {
+//                System.out.println("QTD files: " + fileList.size());
+//            }
+
+
+//            System.out.println("Copying file to Host SFTP");
+//            channelSftp.put(localPath + "/" + fileName, sftpPath);
+//            System.out.println("Copied file to Host SFTP");
 
 //            System.out.println("Copying file from Host SFTP to Local");
 //            channelSftp.get(sftpPath + "/" + fileName, localPath);
@@ -60,21 +103,21 @@ public class TestRestController {
 //            channelSftp.rename(oldDirName, newDirName);
 
             // Changing the file name
-            String oldFileName = sftpPath + "/" + fileName;
-            String newNameFile;
-            String extensionFile;
-            int lastIndex = oldFileName.lastIndexOf('.');
-            if (lastIndex != -1) {
-                newNameFile = oldFileName.substring(0, lastIndex);
-                extensionFile = oldFileName.substring(lastIndex);
-
-                String newFileName = newNameFile + "_DONE" + extensionFile;
-                channelSftp.rename(oldFileName, newFileName);
-            }
+//            String oldFileName = sftpPath + "/" + fileName;
+//            String newNameFile;
+//            String extensionFile;
+//            int lastIndex = oldFileName.lastIndexOf('.');
+//            if (lastIndex != -1) {
+//                newNameFile = oldFileName.substring(0, lastIndex);
+//                extensionFile = oldFileName.substring(lastIndex);
+//
+//                String newFileName = newNameFile + "_DONE" + extensionFile;
+//                channelSftp.rename(oldFileName, newFileName);
+//            }
 
             // Remove file
-            String file = "text1.txt";
-            channelSftp.rm(sftpPath + file);
+//            String file = "text1.txt";
+//            channelSftp.rm(sftpPath + file);
 
             // Remove directory
 //            String directory = "EMAIL";
@@ -85,7 +128,7 @@ public class TestRestController {
 
             System.out.println("Disconnected from sftp");
         } catch (Exception e) {
-            e.printStackTrace();
+                e.printStackTrace();
             System.out.println("Error: " + e.getMessage());
         }
     }
