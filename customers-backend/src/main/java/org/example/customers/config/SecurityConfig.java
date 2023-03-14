4e1f6e5d24a071f4672227b3e74783ddeb380a93
Rafael <rafael_unis@hotmail.com>
2023-03-13 21:02:49 -0300

    Adjust security config

--- customers-backend/src/main/java/org/example/customers/config/SecurityConfig.java
+++ customers-backend/src/main/java/org/example/customers/config/SecurityConfig.java
@@ -2,11 +2,7 @@ package org.example.customers.config;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableWebSecurity
-@EnableResourceServer
-@EnableAuthorizationServer
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 }
