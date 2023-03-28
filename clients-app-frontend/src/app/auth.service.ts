bc4f7104f1449b6d8ae9cb29e626b4c5da82ad8b
Rafael <rafael_unis@hotmail.com>
2023-03-28 18:12:27 -0300

    Registering user through the front end

--- clients-app-frontend/src/app/auth.service.ts
+++ clients-app-frontend/src/app/auth.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './login/user';
+
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  apiURL: string = environment.apiURLBase + "/api/users";
+
+  constructor(private http: HttpClient) { }
+
+  save(user: User) : Observable<any> {
+    return this.http.post<any>(this.apiURL, user);
+  }
 }
--- clients-app-frontend/src/app/login/login.component.ts
+++ clients-app-frontend/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Route, Router } from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +14,11 @@ export class LoginComponent {
   password: string;
   loginError: boolean;
   signingUp: boolean;
+  successMessage: string;
 
   constructor(
-    private router: Router) { }
+    private router: Router,
+    private authService: AuthService) { }
 
   onSubmit() {
     this.router.navigate(['/home']);
@@ -28,6 +32,22 @@ export class LoginComponent {
   unsubscribe() {
     console.log("unsubscribe()")
     this.signingUp = false;
+  }
+
+  registerUser() {
+    console.log("register()")
+    const user: User = new User();
+    user.username = this.username;
+    user.password = this.password;
+    this.authService
+      .save(user)
+      .subscribe(response => {
+        this.successMessage = "Successful registration! Log in."
+        this.loginError = false;
+      }, error => {
+        this.loginError = true;
+        this.successMessage = null;
+      });
   }
 
 }
