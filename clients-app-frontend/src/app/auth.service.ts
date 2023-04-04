6f606f5bf328ef66f733efacc8da0d10f294316d
Rafael <rafael_unis@hotmail.com>
2023-04-04 11:36:18 -0300

    Install JwtHelperService

--- clients-app-frontend/src/app/auth.service.ts
+++ clients-app-frontend/src/app/auth.service.ts
@@ -5,6 +5,8 @@ import { User } from './login/user';
 
 import { environment } from '../environments/environment';
 
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,26 +16,41 @@ export class AuthService {
   tokenURL: string = environment.apiURLBase + environment.getTokenURL;
   clientID: string = environment.clientId;
   clientSecret: string = environment.clientSecret;
+  jwtHelper: JwtHelperService = new JwtHelperService();
 
   constructor(private http: HttpClient) { }
 
+  getToken() {
+    const tokenString = localStorage.getItem('access_token');
+    if (tokenString) {
+      const token = JSON.parse(tokenString).access_token;
+      return token;
+    }
+    return null;
+  }
+
   isAuthenticated(): boolean {
+    const token = this.getToken();
+    if (token) {
+      const isExpired = this.jwtHelper.isTokenExpired(token);
+      return !isExpired;
+    }
     return false;
   }
 
-  save(user: User) : Observable<any> {
+  save(user: User): Observable<any> {
     return this.http.post<any>(this.apiURL, user);
   }
 
-  tryLogin(username: string, password: string) : Observable<any> {
+  tryLogin(username: string, password: string): Observable<any> {
     const params = new HttpParams()
-      .set('username', username)  
+      .set('username', username)
       .set('password', password)
       .set('grant_type', 'password')
 
     const headers = {
       'Authorization': 'Basic ' + btoa(`${this.clientID}:${this.clientSecret}`),
-      'Content-Type': 'application/x-www-form-urlencoded' 
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
     console.log("tryLogin()")
     return this.http.post(this.tokenURL, params.toString(), { headers });
