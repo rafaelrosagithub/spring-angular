9ff9423ac8b73954ceb4cf2116f1260ea56560f2
Rafael <rafael_unis@hotmail.com>
2023-05-09 18:03:43 -0300

    Contact form

--- agenda-app/src/app/app.module.ts
+++ agenda-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInput, MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +20,8 @@ import { ContactService } from './contact.service';
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
   providers: [
