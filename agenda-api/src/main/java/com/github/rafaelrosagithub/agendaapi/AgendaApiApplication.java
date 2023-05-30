978f86b53c5d31c5dc2eb00a888c53398e0ff486
Rafael <rafael_unis@hotmail.com>
2023-05-29 21:38:10 -0300

    Clear import

--- agenda-api/src/main/java/com/github/rafaelrosagithub/agendaapi/AgendaApiApplication.java
+++ agenda-api/src/main/java/com/github/rafaelrosagithub/agendaapi/AgendaApiApplication.java
@@ -1,12 +1,7 @@
 package com.github.rafaelrosagithub.agendaapi;
 
-import com.github.rafaelrosagithub.agendaapi.model.entity.Contact;
-import com.github.rafaelrosagithub.agendaapi.model.repository.ContactRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AgendaApiApplication {
